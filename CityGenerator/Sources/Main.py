0b54cc2e8f4722e4c56d65efbdf5ce9a87e5d5d7
Anton <Anton.francis.roy@gmail.com>
2013-03-01 13:52:28 +0100

    -working on traductor

--- CityGenerator/Sources/Main.py
+++ CityGenerator/Sources/Main.py
@@ -9,22 +9,22 @@ from imp import reload
 import TraductorModule
 reload(TraductorModule)
 from TraductorModule import Traductor 
-boundaries = [Coordinates(0, 0, 0), Coordinates(100, 0, 0), Coordinates(100, 200, 0), Coordinates(100, 100, 0), Coordinates(0, 100, 0)]
+boundaries = [Coordinates(0, 0, 0), Coordinates(100, 0, 0), Coordinates(100, 100, 0), Coordinates(0, 100, 0)]
 field = Field(boundaries)
 city = City(field)
-road = Network([Coordinates(50, 0, 0), Coordinates(50, 100, 0)])
+road = Network([Coordinates(50, 0, 5), Coordinates(50, 50, 5)])
 field.setNetworks([road])
 
 districtBoundaries = [Coordinates(10, 10, 0), Coordinates(40, 10, 0), Coordinates(40, 90, 0), Coordinates(10, 90, 0)]
-district = District(())
+district = District(0)
 district.setBoundaries(districtBoundaries)
 field.setDistricts([district])
 
-block = Block(())
+building = Building(districtBoundaries, 50)
+block = Block(0)
 
 district.setblocks([block])
 block.canBuild = True
-building = Building(districtBoundaries, 50)
 block.setPlots([building])
 
 traductor = Traductor();
