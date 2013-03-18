dbb8606d3fbd10e83eb364039d42f93c644f43f2
Antoine Berry <aberry@sophiacom.fr>
2013-03-18 13:50:12 +0100

    -merge main

--- CityGenerator/Sources/Main.py
+++ CityGenerator/Sources/Main.py
@@ -1,10 +1,13 @@
 from Block import Block
-from Building import Building
 from City import City
 from Coordinates import Coordinates
 from District import District
 from Field import Field
 from Road import Road
+from HabitationBuilding import HabitationBuilding
+from FunctionBuilding import FunctionBuilding
+from Area import Area
+
 
 from imp import reload
 import TraductorModule
@@ -13,23 +16,35 @@ from TraductorModule import Traductor
 
 boundaries = [Coordinates(0, 0, 0), Coordinates(100, 0, 0), Coordinates(100, 100, 0), Coordinates(0, 100, 0)]
 field = Field(boundaries)
-city = City(field)
+city = City("Paris", field)
 road = Road([Coordinates(50, 0, 5), Coordinates(50, 50, 5)])
 field.addNetwork(road)
 
 districtBoundaries = [Coordinates(10, 10, 0), Coordinates(40, 10, 0), Coordinates(40, 90, 0), Coordinates(10, 90, 0)]
-district = District(0)
+district = District()
 district.setBoundaries(districtBoundaries)
 field.setDistricts([district])
 
-building = Building(districtBoundaries, 50)
-block = Block(0)
+habitation = HabitationBuilding(districtBoundaries, 50)
+function = FunctionBuilding(districtBoundaries, 20)
+area = Area(districtBoundaries)
 
-district.setblocks([block])
-block.canBuild = True
-block.setPlots([building])
+blocks=[]
+for i in range(10):
+    block = Block()
+    block.canBuild = True
+    block.setPlots([habitation, function, area])
+    blocks.append(block)
+
+district.setblocks(blocks)
+
 
 traductor = Traductor();
 traductor.draw(city)
 
-city.serialize("firstExempleCity")
+
+city.serialize()
+
+deserializedCity = City.deserialize("CityGen/" + city.getName() + ".cgxml")
+
+
